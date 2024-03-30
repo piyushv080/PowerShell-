1770fcff8e8a484c9a69c4a269441d50ecabaa9e
Piyush Verma <65125758+piyushv080@users.noreply.github.com>
2024-03-31 01:27:22 +0530

    Update and rename mailing list.ps1 to mail_script.ps1

--- mail_script.ps1
+++ mail_script.ps1
@@ -4,10 +4,10 @@
 ##################################################################################################
 $credential = Import-Clixml -Path "C:\Users\Piyush\OneDrive\Desktop\Powershell\pwd_new.xml"
 $health = Get-PhysicalDisk | Sort Size | FT FriendlyName, Size, MediaType, SpindleSpeed, HealthStatus, OperationalStatus -AutoSize |Out-String
-$From = "piyushverma0820@outlook.com"
-$To = "1716510074@kit.ac.in"
+$From = ""
+$To = ""
 $Cc = ""
-$Subject = "Suru kare "
+$Subject = ""
 $Body = "$health"
 $SMTPServer = "smtp.outlook.com"
 $SMTPPort = "587"
